d43ca9b088d31878297499df8f97f526a1c9a002
Quinten <quintenpalmer@gmail.com>
2013-03-07 09:51:57 -0500

    Removed brython directory from web/static

--- web/static/brython/libs/json.js
+++
@@ -1,9 +0,0 @@
-$module =  {
-
-    __getattr__ : function(attr){return this[attr]},
-
-    parse : function(json_obj){return JSON.parse(json_obj)},
-
-    stringify : function(obj){return JSON.stringify(obj)},
-    
-}
