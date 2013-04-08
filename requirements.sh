5652010c78fe1e966e3d8c3956bb75e10b0af7e5
Quinten <quintenpalmer@gmail.com>
2013-04-07 22:14:51 -0400

    Cleanup from last commit
    not using "quinten" username ("developer" instead)
    must be root to run requirements.sh script

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,13 @@
-apt-get install python-pip
-pip install django
-apt-get install python-mysqldb
-#apt-get install java-mysqldb
-apt-get install mysql-server
+#!/bin/bash
+
+ROOT_UID=0
+
+if [ "$UID" -ne "$ROOT_UID" ]; then
+	echo "this script requires to be run as root"
+else
+	apt-get install python-pip
+	pip install django
+	apt-get install python-mysqldb
+	#apt-get install java-mysqldb
+	apt-get install mysql-server
+fi
--- web/root/settings.py
+++ web/root/settings.py
@@ -17,7 +17,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': db_path,  # Or path to database file if using sqlite3.
-        'USER': 'quinten',                      # Not used with sqlite3.
+        'USER': 'developer',                      # Not used with sqlite3.
         'PASSWORD': 'jfjfkdkdlslskdkdjfjf',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
