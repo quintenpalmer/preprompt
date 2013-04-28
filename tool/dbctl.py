851ff2138f1e87c1b29f8666d704e8d6f0f476c1
quinten <quintenpalmer@gmail.com>
2013-04-28 09:51:17 -0400

    Cleaned up database controll tool

--- tool/dbctl.py
+++ tool/dbctl.py
@@ -11,43 +11,52 @@ sys.path.insert(0,pplib_path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
 from django.contrib.auth.models import User
 
+base_path = os.path.join(os.environ['postpromptroot'],'opt','postprompt','tables')
+
+'''
 def load_deck(deck_file):
 	uid = deck_file.split('.')[0]
-	path = os.path.join(os.environ['postpromptroot'],'opt','postprompt','tables','decks',deck_file)
+	path = os.path.join(base_path,'decks',deck_file)
 	f = open(path,'r')
 	for line in f.readlines():
 		key,val = line.split(':')
 		val = val.rstrip('\n')
 	database.insert('play_decks',(int,int,int,str),(None,uid,key,val))
+'''
 
 def reload_cards():
-	database.delete('play_card_names')
+	delete_cards()
+	load_cards()
+def delete_cards():
 	database.delete('play_cards')
 	database.delete('play_starting_cards')
-	load_cards()
-	load_starting_cards()
+	database.delete('play_card_names')
 def load_cards():
-	path = os.path.join(os.environ['postpromptroot'],'opt','postprompt','tables','cards','all.table')
+	path = os.path.join(base_path,'cards','all.table')
 	f = open(path,'r')
 	for key,line in enumerate(f.readlines()):
 		key = key+1
 		val = line.strip()
 		database.insert('play_card_names',(int,str,str),(key,'name',val))
+	def load_starting_cards():
+		path = os.path.join(base_path,'cards','starting.table')
+		f = open(path,'r')
+		line = f.readlines()[0].strip()
+		for val in line.split(','):
+			database.insert('play_starting_cards',(int,int),(None,val))
 	load_starting_cards()
 
-def load_starting_cards():
-	path = os.path.join(os.environ['postpromptroot'],'opt','postprompt','tables','cards','starting.table')
-	f = open(path,'r')
-	line = f.readlines()[0].strip()
-	for val in line.split(','):
-		database.insert('play_starting_cards',(int,int),(None,val))
-
 def reload_users():
-	database.delete('auth_user')
+	delete_users()
 	load_users()
+def delete_users():
+	database.delete('auth_user')
 def load_users():
-	path = os.path.join(os.environ['postpromptroot'],'opt','postprompt','tables','users','all.table')
+	path = os.path.join(base_path,'users','all.table')
 	f = open(path,'r')
+	def add_user(username,password,email):
+		User.objects.create_user(username,email,password)
+		register_add_cards(username)
 	for line in f.readlines():
 		upe = line.strip().split(',')
 		username = upe[0]
@@ -55,49 +64,53 @@ def load_users():
 		email = upe[2]
 		add_user(username,password,email)
 
-def add_user(username,password,email):
-	User.objects.create_user(username,email,password)
-	"""
-	database.insert('auth_user',(int,str,str,int,str,str,str,str,int,int,str),'1','qwer','2013-03-08 03:57:13.167947','0','qwer','','','quintenpalmer@gmail.com','0','1','2013-03-08 03:57:13.167947')
-	"""
-	register_add_cards(username)
-
-def reload_games():
+def delete_games():
 	database.delete('play_games')
 
+def delete_all():
+	delete_games()
+	delete_users()
+	delete_cards()
 
-if len(sys.argv) > 1:
-	if sys.argv[1] == 'load':
-		if sys.argv[2] == 'all':
-			load_cards()
-			load_users()
-	elif sys.argv[1] == 'reload':
-		if sys.argv[2] == 'all':
-			reload_games()
-			reload_cards()
-			reload_users()
-		elif sys.argv[2] == 'cards':
-			reload_cards()
-		elif sys.argv[2] == 'games':
-			reload_games()
-	elif sys.argv[1] == 'delete':
-		sure = raw_input('Are you sure? y/n ')
-		if sure == 'y':
-			if sys.argv[2] == 'all':
-				database.delete('play_decks')
-				database.delete('play_card_names')
-			elif sys.argv[2] == 'cards':
-				database.delete('play_card_names')
-				database.delete('play_card_names')
-			elif sys.argv[2] == 'decks':
-				database.delete('play_decks')
-			elif sys.argv[2] == 'users':
-				database.delete('auth_user')
-			elif sys.argv[2] == 'games':
-				database.delete('play_games')
-	elif sys.argv[1] == 'select':
-		if len(sys.argv) > 3:
-			what = sys.argv[3]
+def load_all():
+	load_cards()
+	load_users()
+
+def reload_all():
+	reload_cards()
+	reload_users()
+
+mapping = {
+	'games' : { 'delete' : delete_games },
+	'cards' : { 'delete' : delete_cards , 'load' : load_cards, 'reload' : reload_cards },
+	'users' : { 'delete' : delete_users , 'load' : load_users, 'reload' : reload_users },
+	'all' : { 'delete' : delete_all , 'load' : load_all, 'reload' : reload_all },
+}
+
+def help_text():
+	ret = "Database Controlling Tool  - Usage :\n"
+	for key,val in mapping.items():
+		ret += '\t%0-8s'%key
+		ret += '[ '
+		for subkey in val.keys():
+			ret += '%0-7s'%(subkey+',')
+		ret = ret[:-1]
+		ret += ' ]'
+		ret += '\n'
+	ret = ret[:-1]
+	return ret
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		if sys.argv[1] in mapping.keys():
+			table = mapping[sys.argv[1]]
+			if sys.argv[2] in table.keys():
+				table[sys.argv[2]]()
+			elif sys.argv[2] == 'select':
+				if len(sys.argv) > 3:
+					what = sys.argv[3]
+				else:
+					what = '*'
+				pprint(database.select(table,what))
 		else:
-			what = '*'
-		pprint(database.select(sys.argv[2],what))
+			print help_text()
